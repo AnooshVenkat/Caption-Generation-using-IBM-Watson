22320ecf6234bbbe0633d723f3d8fc644a83ab32
Anoosh Venkat <anoosh1531@gmail.com>
2020-11-02 18:57:41 +0530

    Text_to_Speech Template.js

--- Text_to_Speech Template.js
+++ Text_to_Speech Template.js
@@ -1,8 +1,8 @@
 /*
-The Function getdata() iterates over all the classes present in the image, as identified by the Visual Recognition Service.
-The necessary classes are appended to the 'voice_message' variable.
-Once all the iterations are completed, the voice_message variable is copied to msg.payload and is then sent to the Watson Text to Speech
-Service, to be played as audio output.
+The Function getdata(data) iterates over/through all the classes present in the image, as identified by the Visual Recognition Service.
+The certain classes are appended to the 'voice_message' variable.
+Once all the iterations are completed, the voice_message variable is copied to msg.payload and is then sent to the Watson Text to Speech Service, 
+to be played as audio output.
 */
 <h3>Voice Service Active</h3>
 <script>
@@ -15,7 +15,7 @@ function getdata(data)
 				$.each(data, function(i){
 				var row = data[i];
 				console.log(row);
-				if(row.score>0.6)  //Accepts any Class with Probability score greater than 0.6 (Minimum Threshold).
+				if(row.score>0.6)  //Accepts all classes with Probability score greater than 0.6 (The Minimum Threshold).
 				{
 				voice_message +=row.class;
 	                	voice_message+=', '
